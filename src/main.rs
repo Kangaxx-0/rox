eb89a8505c9c53781058139d864a61eedd192e6b
Kangaxx-0 <85712372+Kangaxx-0@users.noreply.github.com>
2022-08-31 17:26:23 -0700

    simply arithmetic (#19)
    * simply amrithmetic
    
    * clippy

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,27 @@ fn main() {
     let mut vm = Vm::new();
     vm.initialize();
     let mut chunk = Chunk::new();
-    let index = chunk.push_constant(Value::Number(1.2));
 
-    let code_return = OpCode::Return;
-    let constant = OpCode::Constant(index as u8);
+    // -((1.2 + 3.4) / 5.6)
+    {
+        let index1 = chunk.push_constant(Value::Number(1.2));
+        let constant1 = OpCode::Constant(index1 as u8);
+        chunk.write_to_chunk(constant1, 1);
 
-    chunk.write_to_chunk(constant, 1);
-    chunk.write_to_chunk(code_return, 2);
+        let index2 = chunk.push_constant(Value::Number(3.4));
+        let constant2 = OpCode::Constant(index2 as u8);
+        chunk.write_to_chunk(constant2, 1);
 
-    vm.interpret(chunk).expect("Oops");
+        chunk.write_to_chunk(OpCode::Add, 1);
+
+        let index3 = chunk.push_constant(Value::Number(5.6));
+        let constant3 = OpCode::Constant(index3 as u8);
+        chunk.write_to_chunk(constant3, 1);
+
+        chunk.write_to_chunk(OpCode::Divide, 1);
+        chunk.write_to_chunk(OpCode::Negative, 1);
+
+        chunk.write_to_chunk(OpCode::Return, 3);
+        vm.interpret(chunk).expect("Oops");
+    }
 }
