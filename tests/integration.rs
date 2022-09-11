ea39343b1ab6f1ca807f5b2295636ef1e9f97656
Gaxx <damn.frank2z@gmail.com>
2022-09-11 13:16:50 -0700

    impl bool type

--- tests/integration.rs
+++ tests/integration.rs
@@ -28,6 +28,32 @@ fn run_test_contains(input: &str, expected: &str) -> TestResult {
 
     Ok(())
 }
+
+pub fn fail_test(input: &str, expected: &str) -> TestResult {
+    let mut file = NamedTempFile::new()?;
+    let name = file.path();
+
+    let mut cmd = Command::cargo_bin("rox")?;
+    cmd.arg(name);
+    cmd.env(
+        "PWD",
+        std::env::current_dir().expect("Can't get current dir"),
+    );
+
+    writeln!(file, "{}", input)?;
+
+    let output = cmd.output()?;
+
+    let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+
+    println!("stdout: {}", stdout);
+    println!("stderr: {}", stderr);
+
+    assert!(!stderr.is_empty() && stderr.contains(expected));
+
+    Ok(())
+}
 #[test]
 fn rox_constant_number() -> TestResult {
     run_test_contains("1", "Number(1.0)")
@@ -60,4 +86,9 @@ fn rox_arithmetic_grouping() -> TestResult {
 #[test]
 fn rox_arithmetic_complex_grouping() -> TestResult {
     run_test_contains("-((1+2)*2)", "Number(-6.0)")
+}
+
+#[test]
+fn rox_nagative_string() -> TestResult {
+    fail_test("-a", "unknown type found")
 }
