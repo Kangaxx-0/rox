173d8d3a8a2e9bc1ecc52642ddaea0677a848afe
Kangaxx-0 <85712372+Kangaxx-0@users.noreply.github.com>
2022-09-20 14:36:00 -0700

    stack tests (#55)

--- src/stack.rs
+++ src/stack.rs
@@ -27,7 +27,11 @@ impl Stack {
     }
 
     pub fn peek(&self, distance: usize) -> Option<&Value> {
-        self.values.get(self.values.len() - distance - 1)
+        if distance >= self.values.len() {
+            None
+        } else {
+            self.values.get(self.values.len() - 1 - distance)
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -40,5 +44,72 @@ impl Stack {
 
     pub fn is_empty(&self) -> bool {
         self.values.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push() {
+        let mut stack = Stack::new();
+        stack.push(Value::Number(1.0));
+        stack.push(Value::Number(2.0));
+        stack.push(Value::Number(3.0));
+        assert_eq!(stack.len(), 3);
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut stack = Stack::new();
+        stack.push(Value::Number(1.0));
+        stack.push(Value::Number(2.0));
+        stack.push(Value::Number(3.0));
+        assert_eq!(stack.pop(), Some(Value::Number(3.0)));
+        assert_eq!(stack.pop(), Some(Value::Number(2.0)));
+        assert_eq!(stack.pop(), Some(Value::Number(1.0)));
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut stack = Stack::new();
+        stack.push(Value::Number(1.0));
+        stack.push(Value::Number(2.0));
+        stack.push(Value::Number(3.0));
+        assert_eq!(stack.peek(0), Some(&Value::Number(3.0)));
+        assert_eq!(stack.peek(1), Some(&Value::Number(2.0)));
+        assert_eq!(stack.peek(2), Some(&Value::Number(1.0)));
+        assert_eq!(stack.peek(3), None);
+    }
+
+    #[test]
+    fn test_len() {
+        let mut stack = Stack::new();
+        stack.push(Value::Number(1.0));
+        stack.push(Value::Number(2.0));
+        stack.push(Value::Number(3.0));
+        assert_eq!(stack.len(), 3);
+    }
+
+    #[test]
+    fn test_reset() {
+        let mut stack = Stack::new();
+        stack.push(Value::Number(1.0));
+        stack.push(Value::Number(2.0));
+        stack.push(Value::Number(3.0));
+        stack.reset();
+        assert_eq!(stack.len(), 0);
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let mut stack = Stack::new();
+        assert!(stack.is_empty());
+        stack.push(Value::Number(1.0));
+        assert!(!stack.is_empty());
+        stack.pop();
+        assert!(stack.is_empty());
     }
 }
