df49d96cf8a94fcbcb4fac1fe782fa3262397971
Kangaxx-0 <85712372+Kangaxx-0@users.noreply.github.com>
2022-11-16 16:43:36 -0800

    impl traits for Gc (#140)

--- crates/rox_gc/src/lib.rs
+++ crates/rox_gc/src/lib.rs
@@ -1,10 +1,14 @@
 mod gc;
 mod trace;
 
+use core::fmt;
 use std::{
     cell::Cell,
+    cmp::Ordering,
+    hash::{self, Hasher},
     marker::PhantomData,
     mem,
+    ops::Deref,
     ptr::{self, NonNull},
     rc::Rc,
 };
@@ -101,6 +105,161 @@ impl<T: Trace + ?Sized> Gc<T> {
     #[inline]
     fn inner(&self) -> &GcBox<T> {
         unsafe { &*self.inner_ptr() }
+    }
+}
+
+// Default implementation of `Finalize` for `Gc<T>`.
+impl<T: Trace + ?Sized> Finalize for Gc<T> {}
+
+// Default implementation of `Trace` for `Gc<T>`.
+unsafe impl<T: Trace + ?Sized> Trace for Gc<T> {
+    #[inline]
+    unsafe fn trace(&self) {
+        self.inner().trace_inner();
+    }
+
+    #[inline]
+    unsafe fn root(&self) {
+        assert!(!self.rooted(), "Cannot double root a Gc pointer");
+
+        // Try to get inner before modifying out state. Inner may be inaccessable due to this
+        // method being invoked during the sweeping phase, and we don't want to modify our state
+        // before panicking.
+        self.inner().root_inner();
+
+        self.set_root();
+    }
+
+    #[inline]
+    unsafe fn unroot(&self) {
+        assert!(!self.rooted(), "Cannot double root a Gc pointer");
+
+        // Try to get inner before modifying out state. Inner may be inaccessable due to this
+        // method being invoked during the sweeping phase, and we don't want to modify our state
+        // before panicking.
+        self.inner().unroot_inner();
+
+        self.clear_root();
+    }
+
+    #[inline]
+    fn finalize_glue(&self) {
+        Finalize::finalize(self);
+    }
+}
+
+impl<T: Trace + ?Sized> Clone for Gc<T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        unsafe {
+            self.inner().root_inner();
+            let gc = Gc {
+                ptr_root: Cell::new(self.ptr_root.get()),
+            };
+            gc.set_root();
+            gc
+        }
+    }
+}
+
+impl<T: Trace + ?Sized> Deref for Gc<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &T {
+        &self.inner().value()
+    }
+}
+
+impl<T: Trace + ?Sized> Drop for Gc<T> {
+    #[inline]
+    fn drop(&mut self) {
+        unsafe {
+            self.inner().unroot_inner();
+        }
+    }
+}
+
+impl<T: Trace + ?Sized + fmt::Debug> fmt::Debug for Gc<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(&**self, f)
+    }
+}
+
+impl<T: Trace + ?Sized + fmt::Display> fmt::Display for Gc<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(&**self, f)
+    }
+}
+
+impl<T: Trace + ?Sized + PartialEq> PartialEq for Gc<T> {
+    #[inline(always)]
+    fn eq(&self, other: &Self) -> bool {
+        **self == **other
+    }
+}
+
+impl<T: Trace + ?Sized + Eq> Eq for Gc<T> {}
+
+impl<T: Trace + ?Sized + PartialOrd> PartialOrd for Gc<T> {
+    #[inline(always)]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        (**self).partial_cmp(&**other)
+    }
+
+    #[inline(always)]
+    fn lt(&self, other: &Self) -> bool {
+        **self < **other
+    }
+
+    #[inline(always)]
+    fn le(&self, other: &Self) -> bool {
+        **self <= **other
+    }
+
+    #[inline(always)]
+    fn gt(&self, other: &Self) -> bool {
+        **self > **other
+    }
+
+    #[inline(always)]
+    fn ge(&self, other: &Self) -> bool {
+        **self >= **other
+    }
+}
+
+impl<T: Trace + ?Sized + Ord> Ord for Gc<T> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        (**self).cmp(&**other)
+    }
+}
+
+impl<T: Trace + ?Sized + hash::Hash> hash::Hash for Gc<T> {
+    #[inline]
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (**self).hash(state)
+    }
+}
+
+impl<T: Trace> From<T> for Gc<T> {
+    #[inline]
+    fn from(value: T) -> Self {
+        Self::new(value)
+    }
+}
+
+impl<T: Trace + ?Sized> std::borrow::Borrow<T> for Gc<T> {
+    #[inline]
+    fn borrow(&self) -> &T {
+        &**self
+    }
+}
+
+impl<T: Trace + ?Sized> std::convert::AsRef<T> for Gc<T> {
+    #[inline]
+    fn as_ref(&self) -> &T {
+        &**self
     }
 }
 
